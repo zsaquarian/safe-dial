ffa9b23847c61ad2671c48e0d233dc3377d9f490
Nahos <zsaquar1an.42@gmail.com>
2023-10-21 19:45:09 +0530

    fix: make build work

--- src/lib/genToken.ts
+++ src/lib/genToken.ts
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
-import AccessToken, { VoiceGrant } from 'twilio/lib/jwt/AccessToken';
+import AccessToken from 'twilio/lib/jwt/AccessToken';
 import {
   TWILIO_ACCOUNT_SID,
   TWILIO_API_KEY,
   TWILIO_API_SECRET,
   TWIML_APP_SID
 } from '$env/static/private';
+
+const { VoiceGrant } = AccessToken;
 
 export const genToken = () => {
   const identity = uuidv4();
